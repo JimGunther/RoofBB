10d42e7bf8a3f757a4f2e0aa4b27b4bdc4478b1a
Jim Gunther <164523992+JimGunther@users.noreply.github.com>
2024-08-27 20:29:21 +0100

    Delete Dialogs.py

--- Dialogs.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-from StoreComm import StoreComm
-
-# --------------------------------------------------- Version of 03/06/2024 -------------------------------------------------------------------------
-
-class ArchvDlg(Toplevel):
-    # add here
-    pass
